3a6bac5baf5ef87880a9247e1eed8a71e03019b8
CynicalOne <cynicalonemusic@gmail.com>
2020-02-26 00:16:42 -0800

    Lab 1 completed lab 1

---
+++ lab1/README.md
@@ -0,0 +1,8 @@
+# Comp-322
+
+First Commit 2-26-2020
+	Program is the "hello world" of forking. program creates a child process, waits for child to finish processes, then prints its own processess. 
+	Completed lab 1
+	
+	
+	Project Status: Completed.
---
+++ lab1/lab1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>	
+#include <sys/types.h>	
+#include <unistd.h>
+#include <sys/wait.h>	
+#include <stdlib.h>
+#include <sys/times.h>
+
+
+int main() {
+	time_t* startTime = 0;
+	pid_t child;
+	int status;
+	struct tms timer;
+	
+	printf("START: %ld\n",time(startTime));
+	child = fork();	//starts the fork process
+
+	
+	if(child > 0){ //parent process
+		waitpid(child, &status, 0);	//waits for child to finish
+		times(&timer);	//measures elapsed time
+		
+		printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d \n", getppid(), getpid(), child, status);
+		
+		printf("USER: %ld, SYS: %ld\n", timer.tms_utime, timer.tms_stime );
+		printf("CUSER: %ld, CSYS: %ld\n", timer.tms_cutime, timer.tms_cstime );
+		
+		
+		printf("STOP: %ld \n", time(startTime));
+	}
+	else if(child == 0) {	//child process
+		printf("PPID: %d, PID: %d \n", getppid(), getpid());
+	}
+	else {
+		printf("Error, child not created.");
+	}
+	
+	return 0;
+}
