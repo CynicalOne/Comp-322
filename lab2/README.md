f9356f44fd5d06580791c2046b76e2e743ee5b53
CynicalOne <cynicalonemusic@gmail.com>
2020-03-14 13:12:57 -0700

    Added tube tube currently has a problem with the read end of the pipe

--- lab2/README.md
+++ lab2/README.md
@@ -6,3 +6,10 @@ First Commit 3-12-2020
 	
 	
 	Project Status:Completed launch, Tube is in progress
+
+Second Commit 3-14-2020
+	Added tube. memory is allocated for the arguments for both commands. Currently, child1 process feeds the output into the write end of the pipe, but i can't figure out how to utilize the read end to execute process 2. Tests have been done using linux commands "/bin/cat <text file>" piped with "grep <word in file>". process1 is able to do the first command, but is unable to grep correctly as there is an issue with the piping, even though stdin for process2 has been dup2'd with the read end of the pipe.
+	Currently, all the steps asked for in the project specs have been implemented, but there is an issue with the read end of the interprocess communication in child2.
+	
+	
+	Project Status:Completed launch, Tube has logical or syntactical errors in the read end of the pipe
--- lab2/Tube.c
+++ lab2/Tube.c
@@ -1,38 +1,108 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+	//storage vars
 	pid_t child1;
 	pid_t child2;
-	int fd[2];
+	int pipefd[2];
+	int arg1len = 0;
+	int arg2len = 0;
+	int status1;
+	int status2;
 	
-	pipe(fd);
+	if(pipe(pipefd) == -1){
+		perror("pipe error");
+		exit(1);
+	}
 	
-	child1 = fork();	//parent creates first child
-	
-	
-	if(child1 > 0) {	//parent process
-		child2 = fork();	//parent creates second child
-		
-		if(child2 > 0) {	//parent process
-			fprintf(stderr,"CPID1: %d\nCPID2:  %d\n", child1, child2);	//prints PID of both children on stderr
-			close(fd);	//closes access to pipe
+	// retrieving the args for the first command
+	for(int i = 1; i < argc; i++){
+		if((strcmp(argv[i], ",") == 0) || (argv[i] == NULL)){
+			break;
 		}
-		else if(child2 == 0) {	//child 2 process
-			dup2(
+		arg1len++;
+	}
+	// memory allocation for list of args for first command
+	char** args1 = (char**) calloc(arg1len, sizeof(char*));
+	for(int i = 0; i < arg1len; i++){
+		args1[i] = (char*) calloc(50, sizeof(char));
+	}
+	// copies first set of args into args1
+	for(int i = 1; i <= arg1len; i++){
+		strcpy(args1[i - 1], argv[i]);
+	}
+	
+	// retrieving args for the second command
+	for(int i = arg1len + 2; i < argc; i++){
+		if((strcmp(argv[i], ",") == 0) || (argv[i] == NULL)){
+			break;
 		}
-		else {
-			printf("Error, second child not created."
+		arg2len++;
+	}
+	// memory allocation for the list of args for second command
+	char** args2 = (char**) calloc(arg2len, sizeof(char*));
+	for(int i = 0; i < arg2len; i++){
+		args2[i] = (char*) calloc(50, sizeof(char));
+	}
+	// copies second set of args into args2
+	for(int i = arg1len + 2, j = 0; i < argc; i++){
+		strcpy(args2[j], argv[i]);
+		j++;
+	}
+	
+	
+	child1 = fork();
+	
+	if(child1 == 0 ){	//child1 process
+		//take input from stdin (already done)
+		//replace stdout with write part of pipe
+		dup2(pipefd[1], 1);
+		//close fds
+		close(pipefd[0]);
+		close(pipefd[1]);
+		execve(args1[0], args1, NULL);
+		perror("bad child1 exec:");
+	}
+	else if(child1 > 0){	//parent process
+		//parent closes access to the pipe
+		close(pipefd[0]);
+		close(pipefd[1]);
+		child2 = fork();
+		if(child2 ==0){	//child 2 process
+			//replace stdin with read part of pipe
+			//output to stdout (already done)
+			dup2(pipefd[0], 0);
+			close(pipefd[0]);
+			close(pipefd[1]);
+			execve(args2[0], args2, NULL);
+			perror("bad child2 exec:");
+		}
+		else if(child2 > 0){	//parent process
+			waitpid(child1, &status1, 0);	//child1 process prints into the pipe, doesn't matter for race condition
+			//order printed correctly to ensure no race condition issues
+			fprintf(stderr, "CPID1: %d\n", child1);
+			fprintf(stderr, "CPID2: %d\n", child2);
+			waitpid(child2, &status2, 0);
+			fprintf(stderr,"RETVAL1: %d\n", status1);
+			fprintf(stderr, "RETVAL2: %d\n", status2);
+		}
+		else{
+			printf("Error, second child not created");
+			exit(1);
 		}
 	}
-	else if(child1 == 0) {	//child 1 process
-	
-	}
-	else {
-		printf("Error, first child not created.");
+	else{
+		printf("Error, first child not created");
+		exit(1);
 	}
 	
 	return 0;
 }
-	
